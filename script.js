ebb56ca581a25e463bf14dce5f2d389d75e73f13
Calebe <calebe.mello@escola.pr.gov.br>
2025-03-31 08:22:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
-const aumentarFonteBotao = document.getElementById('aumentar-fonte');
-const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-let tamanhoAtualFonte = 1;
-
-aumentarFonteBotao.addEventListener('click',function(){
-tamanhoAtualFonte += 0.1;
-document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-})
-
-diminuirFonteBotao.addEventListener('click',function(){
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    
     })
 
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
 })
